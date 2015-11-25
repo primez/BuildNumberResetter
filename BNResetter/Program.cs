50ced298eef8dd155c911c6263f31ffc100fa126
primez <vladnleonov@gmail.com>
2015-11-25 21:44:04 +0200

    Initial commit

---
+++ BNResetter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace BNResetter
+{
+    class Program
+    {
+        private const string ProjectsPath = @"C:\TC\systemprofile\.BuildServer\config\projects";
+
+        static void Main()
+        {
+            try
+            {
+                var files = Directory.GetFiles(ProjectsPath, "*buildNumbers.properties", SearchOption.AllDirectories);
+
+                Console.WriteLine($"Found {files.Length} files:");
+
+                foreach (var file in files)
+                {
+                    Console.WriteLine($"Resetting build number for {Path.GetFileName(file)}");
+                    var text = File.ReadAllLines(file);
+
+                    var newText = text
+                        .Select(line => line.Contains("next.build=") ? "next.build=1" : line)
+                        .ToArray();
+
+                    File.WriteAllLines(file, newText);
+                }
+
+                Console.WriteLine("=======================");
+                Console.WriteLine("Done");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
